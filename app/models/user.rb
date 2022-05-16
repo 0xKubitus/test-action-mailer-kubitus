086d1b3a02fa8282266a788c343bf73f7ffb6ede
0xKubitus <n.cubilier@gmail.com>
2022-05-16 10:07:53 +0200

    3.2.4 fini

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
-end
+
+  after_create :welcome_send #callback pour lacer la methode welcome_send apres la creation d'un nouveau User
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now # appelle le mailer 'UserMailer' en lui faisant exécuter 'welcome_email' avec, pour seule entrée, l'instance créée (d'où le 'self').
+  end
+
+
+
+end # fin de la clazsse 'User'
