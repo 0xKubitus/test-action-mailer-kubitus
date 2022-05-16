5743bc682cca05c0d421ed5bc584e718fa5c25b1
0xKubitus <n.cubilier@gmail.com>
2022-05-16 09:40:32 +0200

    3.2.2 fini

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,18 @@
+class UserMailer < ApplicationMailer
+
+  default from: 'no-reply@website.fr' # permet de renseigner l'adresse par defaut qui envoie les mails de notre app.
+
+  def welcome_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l'email en définissant destinataire et sujet :
+    mail(to: @user.email, subject: "Ecris ici l'objet de ton email !")
+  end
+
+
+
+end # fin de la classe UserMailer
