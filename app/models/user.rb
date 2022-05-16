75a55a35ccace5c507eb42ad2999714b87e520cd
0xKubitus <n.cubilier@gmail.com>
2022-05-16 10:50:28 +0200

    3.3.1.c) OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
 
-  after_create :welcome_send #callback pour lacer la methode welcome_send apres la creation d'un nouveau User
+  after_create :welcome_send #Grâce au callback after_create, on exécute la méthode welcome_send sur l'instance qui vient d'être sauvée en BDD.
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now # appelle le mailer 'UserMailer' en lui faisant exécuter 'welcome_email' avec, pour seule entrée, l'instance créée (d'où le 'self').
   end
+# welcome_send dit, en résumé, "exécute NOW la méthode welcome_email située dans le mailer UserMailer"
 
 
 
